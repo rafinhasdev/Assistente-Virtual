d7ba72cd9b170a70e87e920cff363ef101caf24d
rafinha <heyakilloficial@gmail.com>
2025-07-09 09:30:47 -0300

    Admin n vai?

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Usuarios, Waha, Backlogs
+
+admin.site.register(Usuarios)
+admin.site.register(Backlogs)
+admin.site.register(Waha)
 
 # Register your models here.
--- app/models.py
+++ app/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
+
 class Usuarios(models.Model):
     matricula = models.CharField(max_length=14, unique=True, null=True)
+    email = models.EmailField(unique=True, null=True)
     nome = models.CharField(max_length=255, null=True)
     sobrenome = models.CharField(max_length=255, null=False, blank=False)
+    numero = models.CharField(max_length=20, null=False)
     data_cadastro = models.DateField(auto_now_add=True, null=True)
     data_ultimo_login = models.DateTimeField(auto_now_add=True, null=True)
     
@@ -11,19 +14,20 @@ class Usuarios(models.Model):
         return self.nome
 
 class Backlogs(models.Model):
-    num_versao = models.IntegerField(max_length=20, unique=True, null=True)
+    num_versao = models.IntegerField(unique=True, null=True)
     data_postagem = models.DateField(auto_now_add=True)
     data_alteracao = models.DateTimeField(auto_now_add=True)
     descricao = models.TextField(null=False, blank=False)
+    dev_responsavel = models.CharField(max_length=255, blank=False)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return f"Versionamento: {self.num_versao}::{self.data_alteracao}"
 
 class Waha (models.Model):
     usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE, null=True)
     num_sessao = models.CharField(max_length=255, null=True, unique=True)
-    numero = models.CharField(max_length=20, null=True, unique=True)
-    ultima_alteracao = models.DateField(auto_now_add=True)
+    data_ultima_alteracao = models.DateField(auto_now_add=True)
+    token_api_suap = models.CharField(max_length=255, unique=True, null=True)
 
-    def __str__(self) -> str:
-        return f"Sessão: {self.num_sessao}"
+    def __str__(self):
+        return f"Sessão: {self.usuario.nome}"
